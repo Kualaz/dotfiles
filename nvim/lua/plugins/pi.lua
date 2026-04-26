0ae31d6df78d3dbf9b9990be85345884d9db5dbe
goncalof <x358280@bcpcorp.net>
2026-04-27 00:26:07 +0100

    Configure Pi Neovim keymaps locally

--- nvim/lua/plugins/pi.lua
+++ nvim/lua/plugins/pi.lua
@@ -3,6 +3,20 @@ return {
 	name = "pi-nvim",
 	lazy = false,
 	config = function()
-		require("pi").setup()
+		local pi = require("pi")
+		pi.setup()
+
+		vim.keymap.set("n", "<leader>aa", function()
+			pi.ask({ initial_text = "@this " })
+		end, { desc = "Ask Pi (@this = current line)" })
+		vim.keymap.set("x", "<leader>aa", function()
+			pi.ask({ selection = pi.capture_selection(), initial_text = "@this " })
+		end, { desc = "Ask Pi (@this = selection)" })
+		vim.keymap.set("n", "<leader>ab", function()
+			pi.send_all()
+		end, { desc = "Ask Pi with buffer" })
+		vim.keymap.set("n", "<leader>ap", function()
+			pi.list_sessions()
+		end, { desc = "Pick Pi session" })
 	end,
 }
